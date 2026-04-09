bb9d07f7b4f7b37e3fc9b14e2c34777aea2e85f3
Fiona Stenhouse <fiona.stenhouse@wiltshire.gov.uk>
2026-04-09 21:56:27 +0100

    Create correct App.jsx to React render UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,10 @@ import React from "react";
 
 export default function App() {
 return (
-<div 
-style={{ 
-background: "#eee", 
-minHeight: "100vh", 
-paddingTop: "80px", 
-}}
->
-<h1 style={{ textAlign: "center" }}>UI rebuild</h1>
-
-<div 
-style={{ 
-maxWidth: "900px", 
-margin: "40px auto", 
-position: "relative" 
-}}
->
-<img
-src="/laptop.jpg"
-alt="Laptop"
-style={{ width: "100%, display: "block" }}
-/>
-</div>
+<div style={{ padding: "40px" }}>
+<h1>UI rebuild</h1>
+<p>If you can see this, App.jsx is finally being used.
+</p>
 </div>
 );
 }
