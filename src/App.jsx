1e466265f38d8674af3f189a14f3aa27c376ff9c
Fiona Stenhouse <fiona.stenhouse@wiltshire.gov.uk>
2026-04-09 21:21:13 +0100

    Remove debug banner and reset baseline UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,8 @@ import React from "react";
 
 export default function App() {
 return ( 
-<pre
-style={{
-position: "fixed",
-top: 0,
-left: 0,
-right: 0,
-padding: "40px",
-background: "yellow",
-color: "black",
-zIndex: 999999,
-fontSize: "24px",
-}}
->
-✅ REACT IS RENDERING
-</pre>
+<div style={{ padding: "40px" }}>
+<h1>UI rebuild starts here</h1>
+</div>
 );
 }
